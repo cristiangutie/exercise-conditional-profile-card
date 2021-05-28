edce84c8cf78041758855ac51565626d39c4dc31
cristiangutie <cristian.gutie@gmail.com>
2021-05-28 00:56:56 +0000

    social media URL

--- src/js/index.js
+++ src/js/index.js
@@ -41,16 +41,16 @@ function render(variables = {}) {
     variables.country === null ? "Country" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${
+            <li><a href="https://twitter.com/@${
               variables.twitter
             }"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="${
+            <li><a href="https://github.com/${
               variables.github
             }"><i class="fa fa-github"></i></a></li>
-            <li><a href="${
+            <li><a href="https://linkedin.com/${
               variables.linkedin
             }"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="${
+            <li><a href="https://instagram.com/${
               variables.instagram
             }"><i class="fa fa-instagram"></i></a></li>
           </ul>
